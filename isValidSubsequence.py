b97864c3b9b68af8ee11853d81cce521240863eb
Nat <nlaw.dev@gmail.com>
2021-01-25 15:53:56 -0800

    solved same question using a for loop instead

--- isValidSubsequence.py
+++ isValidSubsequence.py
@@ -1,8 +1,8 @@
 #Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
 
+# Both similar approaches, one uses a for loop, one uses a while loop
 
-
-#O(n) time, 0(1) space
+#O(n) time, O(1) space
 def isValidSubsequence(array, sequence):
     arrIdx = 0
 	seqIdx = 0
@@ -13,3 +13,13 @@ def isValidSubsequence(array, sequence):
 	return seqIdx == len(sequence)
 
 isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])
+ 
+#O(n) time, O(1) space
+def isValidSubsequence(array, sequence):
+	seqIdx = 0
+	for value in array:
+		if seqIdx == len(sequence) :
+			break
+		if sequence[seqIdx] == value:
+			seqIdx += 1
+	return seqIdx == len(sequence)
